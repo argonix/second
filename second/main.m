f186a9e93d567d140fa41b880dc2c9945dd9a1ff
Artur <argonix@Mac-mini-Artur.local>
2014-08-11 03:22:16 +0400

    Initial Commit

---
+++ second/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  second
+//
+//  Created by Artur on 11.08.14.
+//  Copyright (c) 2014 argonix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
